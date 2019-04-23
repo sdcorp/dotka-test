7434698aeebaae2ba443d389ed8b17561de04ad6
Dmitriy Soldatov <dmitrsold@gmail.com>
2019-04-23 11:38:11 +0300

    trying react hooks for handling input change

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const SearchBox = styled.div`
@@ -19,32 +19,29 @@ const SearchBox = styled.div`
 // Пришлось делать Lifting State Up. Ну или лепить поиск прямо в Main Page (но мы же за компонентный подход)
 // Решил показать что я умею это...
 // Редакс бы решил эту проблему. Но его внедрять было слишком долго
-class Search extends Component {
-  state = {
-    inputValue: ''
-  };
 
-  handleInputChange = e => this.setState({ inputValue: e.target.value });
-
-  render() {
-    const { searchHero } = this.props;
-    const { inputValue } = this.state;
-    return (
-      <SearchBox>
-        <h2>Search</h2>
-        <input type="text" onChange={this.handleInputChange} value={inputValue} placeholder="Enter a hero name" />
-        <button onClick={() => searchHero(inputValue)}>Find</button>
-        <button
-          onClick={() => {
-            searchHero('');
-            this.setState({ inputValue: '' });
-          }}
-        >
-          Reset
-        </button>
-      </SearchBox>
-    );
-  }
-}
+const Search = ({ searchHero }) => {
+  const [inputValue, setInputValue] = useState('');
+  return (
+    <SearchBox>
+      <h2>Search</h2>
+      <input
+        type="text"
+        onChange={e => setInputValue(e.target.value)}
+        value={inputValue}
+        placeholder="Enter a hero name"
+      />
+      <button onClick={() => searchHero(inputValue)}>Find</button>
+      <button
+        onClick={() => {
+          searchHero('');
+          setInputValue('');
+        }}
+      >
+        Reset
+      </button>
+    </SearchBox>
+  );
+};
 
 export default Search;
